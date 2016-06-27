1971fdb0c538baf57065cd7165793267c7b91306
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-06-26 22:49:09 -0600

    Rename a file

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         // Metadata.
-        pkg: grunt.file.readJSON('catholic-calendar.jquery.json'),
+        pkg: grunt.file.readJSON('catholic-calendar.json'),
         banner: '/*\n' +
                 '* <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
                 '<%= pkg.homepage ? "* " + pkg.homepage : "" %>\n' +
